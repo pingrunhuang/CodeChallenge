bd3b0fa888831b4355c071989d893b2b2d04b8ed
pingrunhuang <pingrunhuang@gmail.com>
2018-04-11 20:13:21 +0800

    287 and 524

---
+++ leetcode/python_solution/524.py
@@ -0,0 +1,26 @@
+
+
+class Solution:
+    def findCommon(self, s1, s2):
+        n1 = len(s1)
+        n2 = len(s2)
+        if n1==0 or n2==0:
+            return 0
+        p1 = 0
+        p2 = 0
+        while p1<n1 and p2<n2:
+            
+
+    def findLongestWord(self, s, d):
+        """
+        :type s: str
+        :type d: List[str]
+        :rtype: str
+        """
+        for word in d:
+
+
+
+
+if __name__ == "__main__":
+    solution = Solution()

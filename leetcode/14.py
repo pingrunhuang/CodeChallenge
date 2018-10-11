7242c3ee6c06c65ee0a5d4f2cf955a4b2f62f5c8
frank <pingrunhuang@126.com>
2018-10-11 08:30:59 +0800

    3sum

---
+++ leetcode/14.py
@@ -0,0 +1,24 @@
+class Solution:
+    def longestCommonPrefix(self, strs):
+        """
+        :type strs: List[str]
+        :rtype: str
+        """
+        if len(strs)==0:
+            return ''
+        shortest_len = min(len(x) for x in strs)
+        common_prefix = ""
+        for col in range(shortest_len):
+            prev_char = strs[0][col]
+            for row in range(len(strs)):
+                if strs[row][col]!=prev_char:
+                    return common_prefix
+            common_prefix+=strs[0][col]
+        return common_prefix
+
+if __name__ == "__main__":
+    s = Solution()
+    t1 = ["flower","flow","flight"]
+    print(s.longestCommonPrefix(t1))
+    t2 = ["dog","racecar","car"]
+    print(s.longestCommonPrefix(t2))
